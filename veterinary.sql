c27418f135bf5b3601c712968ea975a6648944fe
Rensuuuu <rencesamontanez@gmail.com>
2023-11-10 12:42:18 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,23 @@
 CREATE DATABASE veterinary;
+
+CREATE TABLE owners(
+	ownerid INT PRIMARY KEY,
+	ofirstname VARCHAR(50) NOT NULL,
+	olastname VARCHAR(50) NOT NULL,
+	address VARCHAR(100) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	phone VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE animals(
+	animalid INT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	species VARCHAR(50) NOT NULL,
+	breed VARCHAR(50) NOT NULL,
+	dateofbirth DATE,
+	gender VARCHAR(10) NOT NULL,
+	color VARCHAR(50) NOT NULL,
+	ownerid INT,
+	foreign key (ownerid) references owners(ownerid)
+);
+
